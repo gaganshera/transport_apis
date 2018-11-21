281f60f7e183c23282d905e8ed15850c6a44f3be
Gaganjot Singh <gagan.shera@gmail.com>
2018-11-21 13:48:31 +0530

    remove google key

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-    googleMapsKey: 'AIzaSyAK6T0IVncfLUjQQgRAuuQWnMEMwtQaOgU',
+    googleMapsKey: '',
     defaultPort: 8080,
     mongodbConnection: "mongodb://mongo/transport_apis",
     orderStatuses: { 'UNASSIGNED': 'UNASSIGNED', 'TAKEN': 'TAKEN' },
